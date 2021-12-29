7b08597811f36e313080d16406d94bc5777337a0
lorincet <lorincet@gmail.com>
2021-12-29 12:30:29 +0100

    Initial commit

---
+++ src/test/java/tech/satic/demo/gatewayservice/DemoGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.satic.demo.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
